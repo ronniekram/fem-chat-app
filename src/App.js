e52f3fbaed807d181875f6080da98b78ef319361
Arynn Boniface <arynn.boniface@gmail.com>
2021-10-06 16:58:09 -0400

    all styling is pretty much donezo except radio & input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Phone from './components/phone/Phone';
+import Info from './components/Info';
 import styles from './assets/styles/app.module.scss';
 
 const App = () => {
@@ -7,7 +8,8 @@ const App = () => {
     <>
       <div className={styles.darkPill}></div>
       <div className={styles.lightPill}></div>
-      <Phone />
+        <Phone />
+        <Info />
     </>
   );
 };
