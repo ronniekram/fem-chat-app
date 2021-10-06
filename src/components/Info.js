c3bd6469c78d5e0a466963d2925ca774f49cb1f2
Arynn Boniface <arynn.boniface@gmail.com>
2021-10-06 16:27:19 -0400

    phone almost complete. needs radio button and input styling *gag*

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import styles from '../assets/styles/components/info.module.scss';
 
 const Info = () => {
   return (
-    <div>
-      
+    <div className={styles.container}>
+      <h1>Simple booking</h1>
+
+      <p>
+        Stay in touch with our dog walkers through the chat interface. This makes it easy to discuss arrangements and make bookings. Once the walk has been completed you can rate your walker and book again all through the chat.
+      </p>
     </div>
   );
 };
