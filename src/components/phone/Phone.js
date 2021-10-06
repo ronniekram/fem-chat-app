4b91e1a4483ae749c0e128e03ced6c58cab8b2e9
Arynn Boniface <arynn.boniface@gmail.com>
2021-10-05 21:31:23 -0400

    adding screen component, bg images working

--- src/components/phone/Phone.js
+++ src/components/phone/Phone.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import styles from '../../assets/styles/components/phone.module.scss';
 
 const Phone = () => {
   return (
-    <div>
+    <div className={styles.phone}>
       
     </div>
   );
 };
 
 export default Phone;
+
+// width: 247, height: 505
